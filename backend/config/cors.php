44ea6b3d8656735aa48b285bf993fcada78df36e
thepie18204 <neothepie@gmail.com>
2025-12-09 21:25:52 +0700

    add config to deploy

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,19 +1,30 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie', '*'],
+    // 'paths' => ['api/*', 'sanctum/csrf-cookie', '*'],
 
-    'allowed_origins' => [
-        'http://localhost:3000',
-        'http://127.0.0.1:3000',
-        'http://localhost',
-        'http://localhost:8000',
-        'http://127.0.0.1:8000',
-    ],
+    // 'allowed_origins' => [
+    //     'http://localhost:3000',
+    //     'http://127.0.0.1:3000',
+    //     'http://localhost',
+    //     'http://localhost:8000',
+    //     'http://127.0.0.1:8000',
+    // ],
 
-    'supports_credentials' => true,
+    // 'supports_credentials' => true,
+    // 'allowed_methods' => ['*'],
+    // 'allowed_headers' => ['*'],
+    // 'exposed_headers' => [],
+    // 'max_age' => 0,
+       'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
+    'allowed_origins' => [
+        env('FRONTEND_URL', 'http://localhost:3000'),
+        'https://*.vercel.app',
+    ],
+    'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
+    'supports_credentials' => true,
 ];
